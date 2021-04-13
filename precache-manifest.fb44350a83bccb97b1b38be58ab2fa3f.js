869507ad1c493c5a57bdd3220bf965a7546650cb
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-04-13 22:18:47 +0300

    Updates

---
+++ precache-manifest.fb44350a83bccb97b1b38be58ab2fa3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c110373cc23e30e48f9fcf26455b48",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "8d987c550ed90d9655af",
+    "url": "/TodoList/static/css/main.334bc216.chunk.css"
+  },
+  {
+    "revision": "9ccae762dbeff285cb42",
+    "url": "/TodoList/static/js/2.de774c9f.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/TodoList/static/js/2.de774c9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d987c550ed90d9655af",
+    "url": "/TodoList/static/js/main.2373e05e.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
