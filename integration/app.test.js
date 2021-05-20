3b07c488e735a85efd214579370abab355e14cce
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-05-21 02:43:31 +0300

    Snapshot (screenshot) testing all components

--- integration/app.test.js
+++ integration/app.test.js
@@ -2,7 +2,54 @@
 describe('addItemForm', () => {
     it('base example, visually looks correct', async () => {
         // APIs from jest-puppeteer
-        await page.goto('http://localhost:9009/iframe.html?id=additemform-component--add-item-form-base-example');
+        await page.goto('http://localhost:9009/iframe.html?id=todolists-additemform--add-item-form-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+
+describe('EditableSpan', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=todolists-editablespan--editable-span-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+
+describe('TaskDone', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=todolists-task--task-done-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+describe('TaskNotDone', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=todolists-task--task-not-done-example&viewMode=story');
+        const image = await page.screenshot();
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot();
+    });
+});
+
+
+describe('AppWithRedux', () => {
+    it('base example, visually looks correct', async () => {
+        // APIs from jest-puppeteer
+        await page.goto('http://localhost:9009/iframe.html?id=todolists-appwithredux--app-with-redux-example&viewMode=story');
         const image = await page.screenshot();
 
         // API from jest-image-snapshot
